4a1e130e19f48a3fef335ce73de3b5a777555857
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-05-03 04:24:09 +0530

    Loggin the messages received

--- internal/message_webhook/receive.go
+++ internal/message_webhook/receive.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	instainterfaces "github.com/TrendsHub/th-backend/pkg/interfaces/instaInterfaces"
+	"github.com/TrendsHub/th-backend/pkg/openai"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,7 @@ func Receive(c *gin.Context) {
 	var message instainterfaces.InstagramMessage
 	if err := c.ShouldBindJSON(&message); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Println(err.Error())
 		return
 	}
 	message.CalcualateMessageType()
@@ -19,7 +21,10 @@ func Receive(c *gin.Context) {
 	// Handle the Instagram message as needed
 	// You can access message fields like message.Sender.ID, message.Message.Text, etc.
 
+	openai.SendMessage(message.Entry[0].Messaging[0].Sender.ID, message.Entry[0].Messaging[0].Message.Text)
+
 	log.Printf("Received Message of Type %s", message.Entry[0].Messaging[0].Type)
+	log.Println("Complete Message", message)
 
 	c.JSON(http.StatusOK, gin.H{
 		"message":   "Instagram webhook received successfully",
