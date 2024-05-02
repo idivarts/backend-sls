e89725f67f6b05b1a108b7e9bc95e92c52e8b4fc
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-05-03 04:16:09 +0530

    Added open ai api key

--- pkg/openai/var.go
+++ pkg/openai/var.go
@@ -1,8 +1,16 @@
 package openai
 
+import (
+	"log"
+	"os"
+)
+
 const (
 	baseURL = "https://api.openai.com/v1"
-	apiKey  = "your_openai_api_key_here"
+)
+
+var (
+	apiKey = "your_openai_api_key_here"
 )
 
 type AssistantID string
@@ -10,3 +18,16 @@ type AssistantID string
 const (
 	ArjunAssistant AssistantID = "asst_3rJKwjfT1VeXRh6KHLg4hQoM"
 )
+
+func init() {
+	envValue := os.Getenv("OPENAI_API_KEY")
+
+	// Check if the environment variable is set
+	if envValue == "" {
+		log.Println("Environment variable is not set")
+	} else {
+		log.Println("Environment variable value:", envValue)
+	}
+
+	apiKey = envValue
+}
