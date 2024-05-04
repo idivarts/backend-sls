69a36734e7fa10b6ef97ef031a6607ee298bbd68
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-05-05 03:27:47 +0530

    Adding logs to find out whats going on

--- internal/message_sqs/handler.go
+++ internal/message_sqs/handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/TrendsHub/th-backend/internal/models"
@@ -16,7 +17,10 @@ import (
 func Handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 	for _, message := range sqsEvent.Records {
 		fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
-		sendMessage(message.Body)
+		err := sendMessage(message.Body)
+		if err != nil {
+			log.Println(err.Error())
+		}
 	}
 	return nil
 }
@@ -32,19 +36,22 @@ func sendMessage(message string) error {
 		return errors.New("Malformed Input")
 	}
 
+	log.Println("Starting Run")
 	err = openai.StartRun(conv.ThreadID, openai.ArjunAssistant)
 	if err != nil {
 		return err
 	}
 	time.Sleep(5 * time.Second)
 
+	log.Println("Getting messaged from thread", conv.ThreadID)
 	msgs, err := openai.GetMessages(conv.ThreadID)
 	if err != nil {
 		return err
 	}
-
+	log.Println("Message received", len(msgs.Data[0].Content), msgs.Data[0].Content[0].Text.Value)
 	aMsg := msgs.Data[0].Content[0].Text
 
+	log.Println("Sending Message", conv.IGSID, aMsg.Value, msgs.Data[0].ID)
 	messenger.SendTextMessage(conv.IGSID, aMsg.Value)
 
 	return nil
