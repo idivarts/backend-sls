6dc6ce46567f3f212f079f2f8e000ff217d5e1df
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-05-16 21:13:19 +0530

    Create apis for subscription and getting subscription

--- facebook-conversations.sh
+++ facebook-conversations.sh
@@ -2,8 +2,12 @@ PAT=EAANaW51FjsgBOwj50iKNnsgxtFn2eSC9jgwlMHHT2JtafZAaulo3sYi2u87t3Lm8riYGguwahnh
 PAGEID=me
 PLATFORM=instagram
 access_token=EAAGDG5jzw5QBO57Niys7D5wRDd74zT4NEZAKZBnHAfuFW5W34nGZCQQRgIOX87VmtXS3XZC0wUxSp8by5TmtuTHjefXjEemS2nACMzy5GPofZAjOZC1AqSzcZB7I7ZAQn3ftj2mrBUXPzNLGCebZBxMZAKUTDZBEZA5j0ZAWYuyhZCAMgxwTXbnU58YqteQZBZBgCNZAD8rma1vqDhpUMDopTx7scnQZDZD
+PAT2=EAANaW51FjsgBOwj50iKNnsgxtFn2eSC9jgwlMHHT2JtafZAaulo3sYi2u87t3Lm8riYGguwahnhp6HYIAZCO0I2pMK1p0ZBmRGnww2BpgZAzCXZCSWiIevsJUy5qM5z6Mhw5LBcLouLZCqv1dCvAjSBZA3eqd11eoWAPXZAEE59ByytvyuR1xFlHQJI7y6WQKN97fwZDZD
 
-curl -i -X GET "https://graph.facebook.com/v19.0/oauth/access_token?grant_type=fb_exchange_token&client_id=425629530178452&client_secret=0babd776dab621585c2370dccec78f2f&fb_exchange_token=$access_token" 
+curl -i -X POST \
+  "https://graph.facebook.com/v19.0/me/subscribed_apps?subscribed_fields=feed&access_token=EAAFB..."
+
+# curl -i -X GET "https://graph.facebook.com/v19.0/oauth/access_token?grant_type=fb_exchange_token&client_id=425629530178452&client_secret=0babd776dab621585c2370dccec78f2f&fb_exchange_token=$access_token" 
 # curl -i -X GET \
 #  "https://graph.facebook.com/v19.0/me/accounts?fields=instagram_business_account&access_token=$access_token"
 # curl -i -X GET "https://graph.facebook.com/v19.0/$PAGEID/conversations?platform=$PLATFORM&fields=name,id,messages\{to,message\}&access_token=$PAT"
---
+++ pkg/messenger/subscribe_app.go
@@ -0,0 +1,77 @@
+package messenger
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const webhook_events = "messages,message_reads"
+
+func SubscribeApp() error {
+	// Convert the message struct to JSON
+	url := baseURL + "/" + apiVersion + "/me/subscribed_apps?subscribed_fields=" + webhook_events + "&access_token=" + pageAccessToken
+	fmt.Println(url)
+	// Make the HTTP request
+	resp, err := http.Post(url, "application/json", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	defer resp.Body.Close()
+
+	rData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	// rStr := string(rData)
+	fmt.Println("Status Code", resp.StatusCode, string(rData))
+
+	// Check the response status
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+type SubscribedAppData struct {
+	Data []struct {
+		Link             string   `json:"link"`
+		Name             string   `json:"name"`
+		ID               string   `json:"id"`
+		SubscribedFields []string `json:"subscribed_fields"`
+	} `json:"data"`
+}
+
+func GetSubscribedApps() (*SubscribedAppData, error) {
+	// Convert the message struct to JSON
+	url := baseURL + "/" + apiVersion + "/me/subscribed_apps?access_token=" + pageAccessToken
+	fmt.Println(url)
+	// Make the HTTP request
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	rData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	// rStr := string(rData)
+	fmt.Println("Status Code", resp.StatusCode, string(rData))
+
+	// Check the response status
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+	rObj := &SubscribedAppData{}
+	err = json.Unmarshal(rData, rObj)
+	if err != nil {
+		return nil, err
+	}
+	return rObj, nil
+}
