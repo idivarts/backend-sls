0e1a2a676e0729884694c58dfcca88c12a4721d3
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-05-26 13:29:24 +0530

    Added reminder details in get reponse

--- internal/business_apis/pages.go
+++ internal/business_apis/pages.go
@@ -8,12 +8,16 @@ import (
 )
 
 type PageUnit struct {
-	Id                 string `json:"id"`
-	Name               string `json:"name"`
-	UserName           string `json:"userName"`
-	OwnerName          string `json:"ownerName"`
-	IsInstagram        bool   `json:"isInstagram"`
-	IsWebhookConnected bool   `json:"isWebHookConnected"`
+	Id                     string `json:"id"`
+	Name                   string `json:"name"`
+	UserName               string `json:"userName"`
+	OwnerName              string `json:"ownerName"`
+	IsInstagram            bool   `json:"isInstagram"`
+	IsWebhookConnected     bool   `json:"isWebHookConnected"`
+	ReminderTimeMultiplier int    `json:"reminderTimeMultiplier"`
+	ReplyTimeMin           int    `json:"replyTimeMin"`
+	ReplyTimeMax           int    `json:"replyTimeMax"`
+	AssistantID            string `json:"assistantId"`
 }
 
 type PagesGetResponse struct {
@@ -54,21 +58,29 @@ func GetPages(c *gin.Context) {
 		}
 		if v.UserID == req.UserID {
 			pagesResp.MyPages = append(pagesResp.MyPages, PageUnit{
-				Id:                 v.PageID,
-				Name:               v.Name,
-				UserName:           v.UserName,
-				OwnerName:          v.OwnerName,
-				IsInstagram:        v.IsInstagram,
-				IsWebhookConnected: v.IsWebhookConnected,
+				Id:                     v.PageID,
+				Name:                   v.Name,
+				UserName:               v.UserName,
+				OwnerName:              v.OwnerName,
+				IsInstagram:            v.IsInstagram,
+				IsWebhookConnected:     v.IsWebhookConnected,
+				ReminderTimeMultiplier: v.ReminderTimeMultiplier,
+				ReplyTimeMin:           v.ReplyTimeMin,
+				ReplyTimeMax:           v.ReplyTimeMax,
+				AssistantID:            v.AssistantID,
 			})
 		} else {
 			pagesResp.OtherPages = append(pagesResp.OtherPages, PageUnit{
-				Id:                 v.PageID,
-				Name:               v.Name,
-				UserName:           v.UserName,
-				OwnerName:          v.OwnerName,
-				IsInstagram:        v.IsInstagram,
-				IsWebhookConnected: v.IsWebhookConnected,
+				Id:                     v.PageID,
+				Name:                   v.Name,
+				UserName:               v.UserName,
+				OwnerName:              v.OwnerName,
+				IsInstagram:            v.IsInstagram,
+				IsWebhookConnected:     v.IsWebhookConnected,
+				ReminderTimeMultiplier: v.ReminderTimeMultiplier,
+				ReplyTimeMin:           v.ReplyTimeMin,
+				ReplyTimeMax:           v.ReplyTimeMax,
+				AssistantID:            v.AssistantID,
 			})
 		}
 	}
