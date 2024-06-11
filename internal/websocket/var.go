0d46e4f7cc64b96c32029b8769ecb9690b61646a
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-06-12 00:22:30 +0530

    Configured websocket table

--- internal/websocket/var.go
+++ internal/websocket/var.go
@@ -4,5 +4,5 @@ import dynamodbhandler "github.com/TrendsHub/th-backend/pkg/dynamodb_handler"
 
 var (
 	dynamoClient = dynamodbhandler.Client
-	tableName    = "webSocketConnections"
+	tableName    = "websocketTable"
 )
