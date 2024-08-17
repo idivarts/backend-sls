677c7c0daa06884cc8a56100f3feb904fd008d13
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-08-17 15:25:45 +0530

    Done all the campaigns apis

--- functions/crowdy_chat/main.go
+++ functions/crowdy_chat/main.go
@@ -10,5 +10,10 @@ func main() {
 	apihandler.GinEngine.GET("/organizations/:orgId", crowdychat.GetOrganizationByID)
 	apihandler.GinEngine.POST("/organizations", crowdychat.CreateOrganization)
 
+	apihandler.GinEngine.GET("/campaigns", crowdychat.GetCampaigns)
+	apihandler.GinEngine.GET("/campaigns/:id", crowdychat.GetCampaignByID)
+	apihandler.GinEngine.POST("/campaigns", crowdychat.CreateCampaign)
+	apihandler.GinEngine.PUT("/campaigns/:id", crowdychat.UpdateCampaign)
+
 	apihandler.StartLambda()
 }
