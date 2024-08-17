80a70db9fce5475f7399173d8e820dd51907e2d6
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-08-17 20:23:27 +0530

    Moved to session apis

--- functions/crowdy_chat/session/main.go
+++ functions/crowdy_chat/session/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	crowdychat "github.com/TrendsHub/th-backend/internal/crowdy_chat"
-	cc_campaigns "github.com/TrendsHub/th-backend/internal/crowdy_chat/campaigns"
 	"github.com/TrendsHub/th-backend/internal/middlewares"
 	apihandler "github.com/TrendsHub/th-backend/pkg/api_handler"
 )
@@ -15,13 +14,6 @@ func main() {
 	sessionRoutes.POST("/organizations", crowdychat.CreateOrganization)
 
 	sessionRoutes.PUT("/profile", crowdychat.UpdateProfile)
-
-	organizationRoutes := sessionRoutes.Group("/main", middlewares.ValidateOrganizationMiddleware())
-
-	organizationRoutes.GET("/campaigns", cc_campaigns.GetCampaigns)
-	organizationRoutes.GET("/campaigns/:id", cc_campaigns.GetCampaignByID)
-	organizationRoutes.POST("/campaigns", cc_campaigns.CreateCampaign)
-	organizationRoutes.PUT("/campaigns/:id", cc_campaigns.UpdateCampaign)
 
 	apihandler.StartLambda()
 }
