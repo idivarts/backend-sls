1585ec1ce1143a3098ec0ac89fb9df024e07835d
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-08-27 03:28:21 +0530

    Added module to test message on server

--- internal/message_webhook/sqs/test.go
+++ internal/message_webhook/sqs/test.go
@@ -1,9 +1,11 @@
 package sqsapp
 
 import (
+	"context"
 	"net/http"
 
 	delayedsqs "github.com/TrendsHub/th-backend/pkg/delayed_sqs"
+	firestoredb "github.com/TrendsHub/th-backend/pkg/firebase/firestore"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +21,15 @@ func SendTestSQSMessage(c *gin.Context) {
 		return
 	}
 
+	_, _, err := firestoredb.Client.Collection("testMessages").Add(context.Background(), map[string]interface{}{
+		"message": testMesage.Message,
+		"delay":   testMesage.Delay,
+	})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	delayedsqs.Send(testMesage.Message, testMesage.Delay)
 	c.JSON(http.StatusOK, gin.H{
 		"message": testMesage,
