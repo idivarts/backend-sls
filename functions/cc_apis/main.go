a86b688ad67e9507bb9eb862f1cb11591d13e753
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-08-29 01:12:50 +0530

    Made some more structure on the api calls

--- functions/cc_apis/main.go
+++ functions/cc_apis/main.go
@@ -14,14 +14,13 @@ func main() {
 	apiV1.POST("/sources/facebook/:sourceId/leads", ccapis.PageWebhook) // We would use this api to create all the leads and fetch there profile
 
 	apiV1.POST("/campaigns/:campaignId", ccapis.GetMessages)           //Initiates the campaigns by creating Assistant
+	apiV1.POST("/campaigns/:campaignId/sync", ccapis.PageSync)         //API to sync either all the connected sources, or specific sources or specific conversations
 	apiV1.POST("/campaigns/:campaignId/sources", ccapis.GetMessages)   // Create API to attach source
 	apiV1.DELETE("/campaigns/:campaignId/sources", ccapis.GetMessages) // Creatae API to Delete the attached source and its conversations
-	apiV1.POST("/campaigns/:campaignId/sync", ccapis.PageSync)         //API to sync either all the connected sources, or specific sources or specific conversations
 
-	apiV1.PUT("/conversations/:conversationId/stop", ccapis.StopConversation) // Make changes in the api to stop tracking the conversation
-
-	apiV1.GET("/facebook/messages/:leadId", ccapis.GetMessages)
-	apiV1.POST("/facebook/messages/:leadId", ccapis.SendMessage)
+	apiV1.PUT("/conversations/:conversationId", ccapis.StopConversation) // Make changes in the api to stop tracking the conversation
+	apiV1.GET("/conversations/:conversationId/messages", ccapis.GetMessages)
+	apiV1.POST("/conversations/:conversationId/messages", ccapis.SendMessage)
 
 	apihandler.StartLambda()
 }
