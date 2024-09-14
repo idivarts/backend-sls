f89b39f2015a78d04fc9e15673fc2c61a3eccf64
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-09-14 21:40:59 +0530

    Minor change on handler

--- src/runHandler.ts
+++ src/runHandler.ts
@@ -1,6 +1,12 @@
 import { SQSEvent, SQSHandler } from "aws-lambda";
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
 
 export const runHandler: SQSHandler = async (event: SQSEvent) => {
+    // openai.beta.threads.runs.stream
     for (const record of event.Records) {
         const { body } = record;
         console.log(`Processing SQS message: ${body}`);
