0cf3e6dc8a6448548aaaa76d35e7644fc2a9ccb0
Rahul Sinha <rahul@idiv.in>
2024-11-02 19:27:01 +0530

    Added attachments

--- functions/s3/main.go
+++ functions/s3/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/TrendsHub/th-backend/internal/middlewares"
+	"github.com/TrendsHub/th-backend/internal/s3/attachments"
+	"github.com/TrendsHub/th-backend/internal/s3/images"
 	"github.com/TrendsHub/th-backend/internal/s3/videos"
 	apihandler "github.com/TrendsHub/th-backend/pkg/api_handler"
 )
@@ -10,8 +12,8 @@ func main() {
 	apiV1 := apihandler.GinEngine.Group("/s3/v1", middlewares.ValidateSessionMiddleware())
 
 	apiV1.POST("/videos", videos.S3UploadHandler)
-	apiV1.POST("/images", videos.S3UploadHandler)
-	apiV1.POST("/attachments", videos.S3UploadHandler)
+	apiV1.POST("/images", images.S3UploadHandler)
+	apiV1.POST("/attachments", attachments.S3UploadHandler)
 
 	apihandler.StartLambda()
 }
