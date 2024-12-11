54ddd8bd0669d574e227e46370bd1741070aaa30
Rahul Sinha <rahul@idiv.in>
2024-12-11 22:30:16 +0530

    Added facebook login

--- internal/models/trendlymodels/social.go
+++ internal/models/trendlymodels/social.go
@@ -1,17 +1,44 @@
 package trendlymodels
 
-import "github.com/idivarts/backend-sls/pkg/messenger"
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/firestore"
+	firestoredb "github.com/idivarts/backend-sls/pkg/firebase/firestore"
+	"github.com/idivarts/backend-sls/pkg/messenger"
+)
 
 type Socials struct {
 	ID           string                      `json:"id" firestore:"id"`
 	Name         string                      `json:"name" firestore:"name"`
 	Image        string                      `json:"image" firestore:"image"`
 	IsInstagram  bool                        `json:"isInstagram" firestore:"isInstagram"`
-	ConnectedId  *string                     `json:"connectedId,omitempty" firestore:"connectedId"`
+	ConnectedID  *string                     `json:"connectedId,omitempty" firestore:"connectedId"`
+	UserID       string                      `json:"userId" firestore:"userId"`
+	OwnerName    string                      `json:"ownerName" firestore:"ownerName"`
 	InstaProfile *messenger.InstagramProfile `json:"instaProfile,omitempty" firestore:"instaProfile"`
 	FBProfile    *messenger.FacebookProfile  `json:"fbProfile,omitempty" firestore:"fbProfile"`
 }
 
 type SocialsPrivate struct {
 	AccessToken *string `json:"accessToken,omitempty"`
+}
+
+func (s *SocialsPrivate) Set(userId, id string) (*firestore.WriteResult, error) {
+	res, err := firestoredb.Client.Collection(fmt.Sprintf("users/%s/socialsPrivate", userId)).Doc(id).Set(context.Background(), s)
+
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (s *Socials) Insert(userId string) (*firestore.WriteResult, error) {
+	res, err := firestoredb.Client.Collection(fmt.Sprintf("users/%s/socials", userId)).Doc(s.ID).Set(context.Background(), s)
+
+	if err != nil {
+		return nil, err
+	}
+	return res, err
 }
--- internal/trendlyapis/facebook.go
+++ internal/trendlyapis/facebook.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/idivarts/backend-sls/internal/middlewares"
+	"github.com/idivarts/backend-sls/internal/models/trendlymodels"
 	"github.com/idivarts/backend-sls/pkg/messenger"
 )
 
@@ -16,19 +18,7 @@ func FacebookLogin(c *gin.Context) {
 		return
 	}
 
-	// sources, err := models.GetSourcesByUserId(organizationID, person.ID)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	return
-	// }
-	// for i := 0; i < len(sources); i++ {
-	// 	sources[i].Status = 0
-	// 	_, err = sources[i].Insert()
-	// 	if err != nil {
-	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// }
+	userId, _ := middlewares.GetUserId(c)
 
 	for _, v := range person.Accounts.Data {
 		lRes, err := messenger.GetLongLivedAccessToken(v.AccessToken)
@@ -40,74 +30,67 @@ func FacebookLogin(c *gin.Context) {
 
 		// var instagram *models.InstagramObject = nil
 		if v.InstagramBusinessAccount.ID != "" {
-			// inst, err := messenger.GetInstagram(v.InstagramBusinessAccount.ID, lRes.AccessToken)
-			// if err != nil {
-			// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			// 	return
-			// }
-			// instaPage := models.Source{
-			// 	OrganizationID:     organizationID,
-			// 	ID:                 inst.ID,
-			// 	Name:               inst.Name,
-			// 	UserID:             person.ID,
-			// 	OwnerName:          person.Name,
-			// 	IsWebhookConnected: false,
-			// 	Status:             1,
-			// 	UserName:           &inst.Username,
-			// 	Bio:                &inst.Biography,
-			// 	SourceType:         models.Instagram,
-			// 	ConnectedID:        &v.ID,
-			// }
-			// _, err = instaPage.Insert()
-			// if err != nil {
-			// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			// 	return
-			// }
-			// instaPPage := models.SourcePrivate{
-			// 	AccessToken: &lRes.AccessToken,
-			// }
-			// _, err = instaPPage.Set(organizationID, inst.ID)
-			// if err != nil {
-			// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			// 	return
-			// }
-			// log.Println("Instagram Saved Accesstoken", instaPPage)
+			insta, err := messenger.GetInstagram(v.InstagramBusinessAccount.ID, lRes.AccessToken)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+			instaPage := trendlymodels.Socials{
+				ID:           insta.ID,
+				Name:         insta.Name,
+				ConnectedID:  &v.ID,
+				UserID:       person.ID,
+				OwnerName:    person.Name,
+				Image:        insta.ProfilePictureURL,
+				IsInstagram:  true,
+				InstaProfile: insta,
+			}
+			_, err = instaPage.Insert(userId)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+			instaPPage := trendlymodels.SocialsPrivate{
+				AccessToken: &lRes.AccessToken,
+			}
+			_, err = instaPPage.Set(userId, insta.ID)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+			log.Println("Instagram Saved Accesstoken", instaPPage)
 		}
 
-		// fb, err := messenger.GetFacebook(lRes.AccessToken)
-		// if err != nil {
-		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		// 	return
-		// }
+		fb, err := messenger.GetFacebook(lRes.AccessToken)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-		// fbPage := models.Source{
-		// 	OrganizationID:     organizationID,
-		// 	ID:                 v.ID,
-		// 	Name:               v.Name,
-		// 	UserID:             person.ID,
-		// 	OwnerName:          person.Name,
-		// 	IsWebhookConnected: false,
-		// 	Status:             1,
-		// 	UserName:           nil,
-		// 	Bio:                nil,
-		// 	SourceType:         models.Facebook,
-		// 	ConnectedID:        &v.InstagramBusinessAccount.ID,
-		// 	// AccessToken:        &lRes.AccessToken,
-		// }
-		// _, err = fbPage.Insert()
-		// if err != nil {
-		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		// 	return
-		// }
-		// fbPPage := models.SourcePrivate{
-		// 	AccessToken: &lRes.AccessToken,
-		// }
-		// _, err = fbPPage.Set(organizationID, v.ID)
-		// if err != nil {
-		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		// 	return
-		// }
-		// log.Println("FB Saved Accesstoken", fbPPage)
+		fbPage := trendlymodels.Socials{
+			ID:          v.ID,
+			Name:        v.Name,
+			UserID:      person.ID,
+			OwnerName:   person.Name,
+			Image:       fb.Picture.Data.URL,
+			ConnectedID: &v.InstagramBusinessAccount.ID,
+			IsInstagram: false,
+			FBProfile:   fb,
+		}
+		_, err = fbPage.Insert(userId)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		fbPPage := trendlymodels.SocialsPrivate{
+			AccessToken: &lRes.AccessToken,
+		}
+		_, err = fbPPage.Set(userId, v.ID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		log.Println("FB Saved Accesstoken", fbPPage)
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Successfully parsed JSON", "user": person})
