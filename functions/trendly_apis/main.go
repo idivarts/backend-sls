f42eb3fa1bdebf02c46afbf0f5408da3c33318c1
Rahul Sinha <rahul@idiv.in>
2024-12-11 21:31:46 +0530

    Made some changes

---
+++ functions/trendly_apis/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/idivarts/backend-sls/internal/middlewares"
+	"github.com/idivarts/backend-sls/internal/trendlyapis"
+	apihandler "github.com/idivarts/backend-sls/pkg/api_handler"
+)
+
+func main() {
+	apiV1 := apihandler.GinEngine.Group("/api/v1", middlewares.ValidateSessionMiddleware())
+
+	apiV1.POST("/socials/facebook", trendlyapis.FacebookLogin)
+
+	apihandler.StartLambda()
+}
