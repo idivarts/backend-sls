f22c754d7573ec2bb1c5aea0b2c883dcfa151a9e
Rahul Sinha <rahul@idiv.in>
2024-12-11 22:59:45 +0530

    Added client Id and secret

--- pkg/messenger/var.go
+++ pkg/messenger/var.go
@@ -1,5 +1,7 @@
 package messenger
 
+import "os"
+
 const (
 	baseURL             = "https://graph.facebook.com"
 	apiVersion          = "v19.0"
@@ -8,6 +10,19 @@ const (
 	// pageAccessTokenOld = "EAANaW51FjsgBOwj50iKNnsgxtFn2eSC9jgwlMHHT2JtafZAaulo3sYi2u87t3Lm8riYGguwahnhp6HYIAZCO0I2pMK1p0ZBmRGnww2BpgZAzCXZCSWiIevsJUy5qM5z6Mhw5LBcLouLZCqv1dCvAjSBZA3eqd11eoWAPXZAEE59ByytvyuR1xFlHQJI7y6WQKN97fwZDZD"
 	platform     = "instagram"
 	TestUserName = "trendshub.ai"
+)
+
+var (
 	ClientID     = "425629530178452"
 	ClientSecret = "0babd776dab621585c2370dccec78f2f"
 )
+
+func init() {
+	clientId := os.Getenv("FB_CLIENT_ID")
+	clientSecret := os.Getenv("FB_CLIENT_SECRET")
+
+	if clientId != "" && clientSecret != "" {
+		ClientID = clientId
+		ClientSecret = clientSecret
+	}
+}
