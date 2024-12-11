8d8161547efc0c1e1220295ae154837878bb50d5
Rahul Sinha <rahul@idiv.in>
2024-12-11 22:15:23 +0530

    Added social Private

--- internal/models/trendlymodels/social.go
+++ internal/models/trendlymodels/social.go
@@ -7,6 +7,11 @@ type Socials struct {
 	Name         string                      `json:"name" firestore:"name"`
 	Image        string                      `json:"image" firestore:"image"`
 	IsInstagram  bool                        `json:"isInstagram" firestore:"isInstagram"`
+	ConnectedId  *string                     `json:"connectedId,omitempty" firestore:"connectedId"`
 	InstaProfile *messenger.InstagramProfile `json:"instaProfile,omitempty" firestore:"instaProfile"`
 	FBProfile    *messenger.FacebookProfile  `json:"fbProfile,omitempty" firestore:"fbProfile"`
+}
+
+type SocialsPrivate struct {
+	AccessToken *string `json:"accessToken,omitempty"`
 }
