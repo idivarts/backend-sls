f53a6db2c127f816e8c7bfd2bac65855e5848ee1
Rahul Sinha <rahul@idiv.in>
2024-12-19 21:16:01 +0530

    Connect instagram added here

--- functions/trendly_apis/main.go
+++ functions/trendly_apis/main.go
@@ -10,7 +10,7 @@ func main() {
 	apiV1 := apihandler.GinEngine.Group("/api/v1", middlewares.ValidateSessionMiddleware(), middlewares.TrendlyMiddleware())
 
 	apiV1.POST("/socials/facebook", trendlyapis.FacebookLogin)
-	apiV1.POST("/socials/instagram", trendlyapis.InstagramAuth)
+	apiV1.POST("/socials/instagram", trendlyapis.ConnectInstagram)
 
 	apiV1.POST("/chat/auth", trendlyapis.ChatAuth)
 	apiV1.POST("/chat/connect", trendlyapis.ChatConnect)
