3350d5f4c1966bbe0ab6893495f8e2a3c57a0392
Rahul Sinha <rahul@idiv.in>
2024-12-31 01:00:40 +0530

    Binded brand members

--- functions/trendly_apis/main.go
+++ functions/trendly_apis/main.go
@@ -16,5 +16,7 @@ func main() {
 	apiV1.POST("/chat/connect", trendlyapis.ChatConnect)
 	apiV1.POST("/chat/channel", trendlyapis.ChatChannel)
 
+	apiV1.POST("/brands/members", trendlyapis.CreateBrandMember)
+
 	apihandler.StartLambda()
 }
