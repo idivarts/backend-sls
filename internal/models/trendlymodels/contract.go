ebd2c529927c59240b027287878ccbd20edf4864
Rahul Sinha <rahul@idiv.in>
2025-01-07 12:50:28 +0530

    Added brandId in the contract

--- internal/models/trendlymodels/contract.go
+++ internal/models/trendlymodels/contract.go
@@ -4,6 +4,7 @@ type Contract struct {
 	UserID          string `json:"userId" firestore:"userId"`
 	ManagerID       string `json:"managerId" firestore:"managerId"`
 	CollaborationID string `json:"collaborationId" firestore:"collaborationId"`
+	BrandID         string `json:"brandId" firestore:"brandId"`
 	StreamChannelID string `json:"streamChannelId" firestore:"streamChannelId"`
 	Status          int    `json:"status" firestore:"status"`
 }
