6fd7aceef82fe14641d787e23f53a0c4de7bd1b1
Rahul Sinha <rahul@idiv.in>
2025-05-06 23:22:01 +0530

    Changed all the names

---
+++ templates/init.go
@@ -0,0 +1,21 @@
+package templates
+
+const (
+	ApplicationAccepted               = "templates/application_accepted.html"
+	ApplicationRejected               = "templates/application_rejected.html"
+	ApplicationSent                   = "templates/application_sent.html"
+	EmailVerification                 = "templates/email_verification.html"
+	CollaborationEndedInfluencer      = "templates/end_contract_brand.html"
+	CollaborationEndedBrand           = "templates/end_contract_influencer.html"
+	InfluencerInvitedToCollab         = "templates/invitation_sent.html"
+	CollaborationQuotationResubmitted = "templates/new_quotation.html"
+	PasswordReset                     = "templates/password_reset.html"
+	PasswordChanged                   = "templates/password_changed.html"
+	CollaborationEndNudged            = "templates/poke_end_contract.html"
+	CollaborationStartRequested       = "templates/poke_start_collaboration.html"
+	CollaborationRatedByInfluencer    = "templates/rating_received.html"
+	CollaborationQuotationRequested   = "templates/revise_quotation.html"
+	CollaborationStarted              = "templates/start_collaboration.html"
+	InfluencerJoined                  = "templates/welcome_influencer.html"
+	BrandCreated                      = "templates/welcome_brand.html"
+)
