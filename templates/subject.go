ec26d8cf8b384a6515e28920bcc3ad07e780e1b8
Rahul Sinha <rahul@idiv.in>
2025-05-06 23:39:56 +0530

    Added subjects

---
+++ templates/subject.go
@@ -0,0 +1,21 @@
+package templates
+
+const (
+	SubjectInfluencerAppliedToCollab  = "An influencer applied to your collaboration!"
+	SubjectApplicationAccepted        = "Your application has been accepted 🎉"
+	SubjectApplicationRejected        = "Your application was not accepted"
+	SubjectQuotationRevisionRequested = "Brand has requested a revision on your quotation"
+	SubjectNewQuotationReceived       = "New quotation received from influencer"
+	SubjectStartCollabReminderToBrand = "Start the collaboration now – influencer is waiting!"
+	SubjectCollaborationStarted       = "Collaboration started – contract is now live"
+	SubjectNudgeToEndContract         = "Influencer nudged to end the contract and rate"
+	SubjectContractEndedForInfluencer = "Contract ended – rate the brand to see their rating"
+	SubjectContractEndedForBrand      = "Contract ended – thanks for rating the influencer"
+	SubjectInfluencerRatedYou         = "New rating received from influencer"
+	SubjectBrandInvitedYouToCollab    = "You've been invited to a collaboration!"
+	SubjectWelcomeNewInfluencer       = "Welcome to Trendly, Creator!"
+	SubjectWelcomeNewBrand            = "Your brand has been created on Trendly 🎉"
+	SubjectEmailVerification          = "Verify your email address for Trendly"
+	SubjectPasswordReset              = "Reset your Trendly password"
+	SubjectPasswordChanged            = "Your Trendly password has been changed"
+)
