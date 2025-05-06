984ee12262aadd581c81fb846489be34deb046a8
Rahul Sinha <rahul@idiv.in>
2025-05-07 01:24:36 +0530

    Added brands

--- internal/trendlyapis/collaborations/invites.go
+++ internal/trendlyapis/collaborations/invites.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/idivarts/backend-sls/internal/middlewares"
+	"github.com/idivarts/backend-sls/internal/models/trendlymodels"
+	"github.com/idivarts/backend-sls/pkg/myemail"
+	"github.com/idivarts/backend-sls/templates"
 )
 
 func SendInvitation(c *gin.Context) {
@@ -12,7 +15,30 @@ func SendInvitation(c *gin.Context) {
 	if userType != "manager" {
 		c.JSON(http.StatusUnauthorized, gin.H{"message": "Only Managers can call this endpoint"})
 	}
-	// collabId := c.Param("collabId")
-	// userId := c.Param("collabId")
+	collabId := c.Param("collabId")
+	userId := c.Param("collabId")
 
+	user := &trendlymodels.User{}
+	err := user.Get(userId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	collab := &trendlymodels.Collaboration{}
+	err = collab.Get(collabId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	// {{.InfluencerName}}  => Name of the influencer receiving the invite
+	// {{.BrandName}}       => Name of the brand sending the invitation
+	// {{.CollabTitle}}     => Title of the collaboration
+	// {{.ApplyLink}}       => Link to view/apply for the collaboration
+
+	myemail.SendCustomHTMLEmail(*user.Email, templates.InfluencerInvitedToCollab, templates.SubjectBrandInvitedYouToCollab, map[string]interface{}{
+		"InfluencerName": user.Name,
+		"CollabTitle":    collab.Name,
+	})
 }
