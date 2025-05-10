791b53cd26d2688c7c608a05a4535e502a8d5415
Rahul Sinha <rahul@idiv.in>
2025-05-10 12:05:36 +0530

    Added some structural functions

--- internal/trendlyapis/collaborations/collaborations.go
+++ internal/trendlyapis/collaborations/collaborations.go
@@ -1,8 +1,20 @@
 package trendlyCollabs
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/idivarts/backend-sls/internal/middlewares"
+)
 
 // Starting a collab | Request to start
 func StartContract(c *gin.Context) {
+	userType := middlewares.GetUserType(c)
+	if userType == "user" {
+		requestToStart(c)
+		return
+	}
+
+}
+
+func requestToStart(c *gin.Context) {
 
 }
--- internal/trendlyapis/collaborations/contracts.go
+++ internal/trendlyapis/collaborations/contracts.go
@@ -1,9 +1,19 @@
 package trendlyCollabs
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/idivarts/backend-sls/internal/middlewares"
+)
 
 // End | request to end Contract
 func EndContract(c *gin.Context) {
+	userType := middlewares.GetUserType(c)
+	if userType == "user" {
+		requestToEndContract(c)
+		return
+	}
+}
+func requestToEndContract(c *gin.Context) {
 
 }
 
