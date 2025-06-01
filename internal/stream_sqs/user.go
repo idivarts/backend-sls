cf0ca208454be7cd2aaa84b87eb75d65ebbe2989
Rahul Sinha <rahul@idiv.in>
2025-06-01 13:22:07 +0530

    Added logic for sending email

--- internal/stream_sqs/user.go
+++ internal/stream_sqs/user.go
@@ -3,7 +3,10 @@ package streamsqs
 import (
 	"fmt"
 
+	"github.com/idivarts/backend-sls/internal/constants"
 	"github.com/idivarts/backend-sls/internal/models/trendlymodels"
+	"github.com/idivarts/backend-sls/pkg/myemail"
+	"github.com/idivarts/backend-sls/templates"
 )
 
 func HandleUnreadMessage(body *StreamWebhook) {
@@ -11,19 +14,38 @@ func HandleUnreadMessage(body *StreamWebhook) {
 		return
 	}
 
-	// for channelID, reminderData := range body.Channels {
-	// 	fmt.Printf("Channel ID: %s\n", channelID)
-	// 	fmt.Printf("Messages: %d\n", len(reminderData.Messages))
-	// 	for _, message := range reminderData.Messages {
-	// 		fmt.Printf("Message ID: %s, Text: %s\n", message.ID, message.Text)
-	// 	}
-	// }
-
 	userId := body.User.ID
 	user := &trendlymodels.User{}
 	err := user.Get(userId)
 	if err != nil {
-		fmt.Printf("Error retrieving user %s: %v\n", userId, err)
+		fmt.Printf("Error retrieving user %s: %s\n", userId, err.Error())
+		return
+	}
+
+	var firstChannelKey string
+	for k := range body.Channels {
+		firstChannelKey = k
+		break
+	}
+	channel := body.Channels[firstChannelKey]
+
+	// Dynamic Variables:
+	// {{.RecipientName}}     => Name of the user receiving the email
+	// {{.CollabTitle}}       => Title of the collaboration
+	// {{.FirstPendingMessage}} => Preview of the first unread message
+	// {{.PendingCount}}      => Total number of unread messages
+	// {{.OpenChatLink}}      => Link to open the chat in the app
+	data := map[string]interface{}{
+		"RecipientName":       user.Name,
+		"CollabTitle":         channel.Channel.Config.Name,                                                      // Assuming the first channel is the relevant one
+		"FirstPendingMessage": channel.Messages[0].Text,                                                         // Preview of the first unread message
+		"PendingCount":        len(channel.Messages),                                                            // Total number of unread messages
+		"OpenChatLink":        fmt.Sprintf("%s/channel/%s", constants.TRENDLY_CREATORS_FE, channel.Channel.CID), // Link to open the chat
+	}
+
+	err = myemail.SendCustomHTMLEmail(*user.Email, templates.MessageReminder, templates.SubjectUnreadMessageReminder, data)
+	if err != nil {
+		fmt.Printf("Error sending email to user %s: %s\n", *user.Email, err.Error())
 		return
 	}
 
