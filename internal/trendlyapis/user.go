818b2546c2a84d3e8e0bd12e1c6b3ba900ff05ca
Rahul Sinha <rahul@idiv.in>
2025-06-04 02:11:06 +0530

    Added functions for user prechecks

--- internal/trendlyapis/user.go
+++ internal/trendlyapis/user.go
@@ -4,12 +4,32 @@ import (
 	"context"
 	"net/http"
 
+	stream_chat "github.com/GetStream/stream-chat-go/v5"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/gin-gonic/gin"
 	"github.com/idivarts/backend-sls/internal/middlewares"
 	"github.com/idivarts/backend-sls/internal/models/trendlymodels"
 	"github.com/idivarts/backend-sls/pkg/firebase/fauth"
+	"github.com/idivarts/backend-sls/pkg/streamchat"
 )
+
+func userPreChecks(user *trendlymodels.User) bool {
+	if user == nil {
+		return false
+	}
+	if user.Settings == nil {
+		user.Settings = &trendlymodels.UserSettings{}
+	}
+	if user.Settings.AccountStatus == nil {
+		user.Settings.AccountStatus = aws.String("Activated")
+	}
+	if *user.Settings.AccountStatus != "Activated" {
+		return false
+	}
+
+	// Add all prechecks like all contracts should be in closed state
+	return true
+}
 
 func DeativateUser(c *gin.Context) {
 	userId, b := middlewares.GetUserId(c)
@@ -22,6 +42,11 @@ func DeativateUser(c *gin.Context) {
 	err := user.Get(userId)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "User not found"})
+		return
+	}
+
+	if !userPreChecks(&user) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User cannot be deleted", "message": "User has active collaborations or contracts"})
 		return
 	}
 
@@ -62,6 +87,11 @@ func DeleteUser(c *gin.Context) {
 		return
 	}
 
+	if !userPreChecks(&user) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User cannot be deleted", "message": "User has active collaborations or contracts"})
+		return
+	}
+
 	if user.Settings == nil {
 		user.Settings = &trendlymodels.UserSettings{
 			AccountStatus: aws.String("Deleted"),
@@ -79,6 +109,12 @@ func DeleteUser(c *gin.Context) {
 	err = fauth.Client.DeleteUser(context.Background(), userId)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Error removing user authentication"})
+		return
+	}
+
+	_, err = streamchat.Client.DeleteUser(context.Background(), userId, stream_chat.DeleteUserWithHardDelete())
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Error removing user on Stream"})
 		return
 	}
 
