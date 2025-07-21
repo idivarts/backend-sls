55c357b25022194df4cbcdf50379fcb3b90c0c33
Rahul Sinha <rahul@idiv.in>
2025-07-21 23:38:01 +0530

    Minor restructuring

--- functions/trendly_apis/main.go
+++ functions/trendly_apis/main.go
@@ -9,9 +9,39 @@ import (
 )
 
 func main() {
-	userApisV1 := apihandler.GinEngine.Group("/api/v1", middlewares.ValidateSessionMiddleware(), middlewares.TrendlyMiddleware("users"))
-	managerApisV1 := apihandler.GinEngine.Group("/api/v1", middlewares.ValidateSessionMiddleware(), middlewares.TrendlyMiddleware("managers"))
+	handleUserAPIs()
+	handleManagerAPIs()
+
 	commonV1 := apihandler.GinEngine.Group("/api/v1", middlewares.ValidateSessionMiddleware(), middlewares.TrendlyMiddleware("common"))
+
+	commonV1.POST("/chat/auth", trendlyapis.ChatAuth)
+	commonV1.POST("/chat/connect", trendlyapis.ChatConnect)
+	commonV1.POST("/chat/channel", trendlyapis.ChatChannel)
+
+	commonV1.POST("/contracts/:contractId", trendlyCollabs.StartContract)   // if called by influencer - ask, else start the contract
+	commonV1.POST("/contracts/:contractId/end", trendlyCollabs.EndContract) // if called by influencer - ask, else end contract
+
+	commonV1.DELETE("/users/deactivate", trendlyapis.DeativateUser)
+	commonV1.DELETE("/users/delete", trendlyapis.DeleteUser)
+
+}
+func handleManagerAPIs() {
+	managerApisV1 := apihandler.GinEngine.Group("/api/v1", middlewares.ValidateSessionMiddleware(), middlewares.TrendlyMiddleware("managers"))
+
+	managerApisV1.POST("/brands/members", trendlyapis.CreateBrandMember)
+
+	managerApisV1.POST("/collaborations/:collabId/invitations/:userId", trendlyCollabs.SendInvitation)
+
+	managerApisV1.POST("/collaborations/:collabId/applications/:userId/:action", trendlyCollabs.ApplicationAction) // accept|reject|revise
+
+	// Managers Explore influencer api
+	managerApisV1.GET("/influencers", matchmaking.GetInfluencers)
+
+	apihandler.StartLambda()
+}
+
+func handleUserAPIs() {
+	userApisV1 := apihandler.GinEngine.Group("/api/v1", middlewares.ValidateSessionMiddleware(), middlewares.TrendlyMiddleware("users"))
 
 	userApisV1.POST("/socials/facebook", trendlyapis.FacebookLogin)
 	userApisV1.POST("/socials/instagram", trendlyapis.ConnectInstagram)
@@ -23,28 +53,8 @@ func main() {
 	// Get Social Medias
 	userApisV1.GET("/socials/medias", trendlyapis.FetchMedias)
 
-	commonV1.POST("/chat/auth", trendlyapis.ChatAuth)
-	commonV1.POST("/chat/connect", trendlyapis.ChatConnect)
-	commonV1.POST("/chat/channel", trendlyapis.ChatChannel)
-	// userApisV1.POST("/chat/notification", trendlyapis.Notify)
-
-	managerApisV1.POST("/brands/members", trendlyapis.CreateBrandMember)
-
-	managerApisV1.POST("/collaborations/:collabId/invitations/:userId", trendlyCollabs.SendInvitation)
 	userApisV1.POST("/collaborations/:collabId/applications/:userId", trendlyCollabs.SendApplication)
 	userApisV1.PUT("/collaborations/:collabId/applications/:userId", trendlyCollabs.EditApplication)
 
-	managerApisV1.POST("/collaborations/:collabId/applications/:userId/:action", trendlyCollabs.ApplicationAction) // accept|reject|revise
-
-	commonV1.POST("/contracts/:contractId", trendlyCollabs.StartContract)   // if called by influencer - ask, else start the contract
-	commonV1.POST("/contracts/:contractId/end", trendlyCollabs.EndContract) // if called by influencer - ask, else end contract
 	userApisV1.POST("/contracts/:contractId/feedback", trendlyCollabs.GiveContractFeedback)
-
-	commonV1.DELETE("/users/deactivate", trendlyapis.DeativateUser)
-	commonV1.DELETE("/users/delete", trendlyapis.DeleteUser)
-
-	// Managers Explore influencer api
-	managerApisV1.GET("/influencers", matchmaking.GetInfluencers)
-
-	apihandler.StartLambda()
 }
