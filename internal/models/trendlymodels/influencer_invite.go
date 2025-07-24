9d1a98a282c6c12b37d5b2eed24c38f271d48a6d
Rahul Sinha <rahul@idiv.in>
2025-07-24 12:43:02 +0530

    Done api to invite the user

---
+++ internal/models/trendlymodels/influencer_invite.go
@@ -0,0 +1,42 @@
+package trendlymodels
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+	firestoredb "github.com/idivarts/backend-sls/pkg/firebase/firestore"
+)
+
+type InfluencerInvite struct {
+	Category    string   `json:"category" firestore:"category"`
+	Reason      string   `json:"reason" firestore:"reason"`
+	CollabType  []string `json:"collabType" firestore:"collabType"`
+	ExampleLink string   `json:"exampleLink" firestore:"exampleLink"`
+	Platforms   []string `json:"platforms" firestore:"platforms"`
+	CollabMode  string   `json:"collabMode" firestore:"collabMode"`
+	BudgetMin   *int     `json:"budgetMin,omitempty" firestore:"budgetMin,omitempty"`
+	BudgetMax   *int     `json:"budgetMax,omitempty" firestore:"budgetMax,omitempty"`
+	Status      int      `json:"status" firestore:"status"` // 0: Pending, 1: Accepted, 2: Rejected
+}
+
+func (b *InfluencerInvite) Get(userID, influencerId string) error {
+	res, err := firestoredb.Client.Collection("users").Doc(userID).Collection("invitations").Doc(influencerId).Get(context.Background())
+	if err != nil {
+		return err
+	}
+
+	err = res.DataTo(b)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func (s *InfluencerInvite) Insert(userId, influencerId string) (*firestore.WriteResult, error) {
+	res, err := firestoredb.Client.Collection("users").Doc(userId).Collection("invitations").Doc(influencerId).Set(context.Background(), s)
+
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
