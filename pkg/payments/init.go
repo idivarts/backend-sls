e7f5a89afed90fab46a1ecf4064213e239bdc894
Rahul Sinha <rahul@idiv.in>
2025-07-30 02:10:06 +0530

    Added a flow to get all needed secrets from gitignore

--- pkg/payments/init.go
+++ pkg/payments/init.go
@@ -1,24 +1,23 @@
 package payments
 
 import (
-	"os"
-
 	razorpay "github.com/razorpay/razorpay-go"
 )
 
 var (
-	Client    *razorpay.Client
-	apiKey    = ""
-	apiSecret = ""
+	Client     *razorpay.Client
+	apiKey     = ""
+	apiSecret  = ""
+	webhookKey = ""
 )
 
 func init() {
-	apiKey = os.Getenv("RAZORPAY_API_KEY")
-	apiSecret = os.Getenv("RAZORPAY_API_SECRET")
+	// Write code to get the items from json file key-secrets.json
 
 	if apiKey == "" || apiSecret == "" {
 		apiKey = "rzp_test_Z9T0fM1E1agkpR"
 		apiSecret = "LaqAVYPBdqdrC4psaoga18nE"
+		webhookKey = "rzp_test_webhook_1234567890"
 	}
 
 	Client = razorpay.NewClient(apiKey, apiSecret)
