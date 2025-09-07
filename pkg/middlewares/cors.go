a0c16b334dc54ec158166ca17943d60919ec2c41
Rahul Sinha <rahul@idiv.in>
2025-09-08 02:43:01 +0530

    Now allowing x user id

--- pkg/middlewares/cors.go
+++ pkg/middlewares/cors.go
@@ -7,7 +7,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Organization-ID, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-USER-ID, X-Organization-ID, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
