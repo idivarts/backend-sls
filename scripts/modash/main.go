46c4249742ebc8bd124f2a150c23190da2b6b2d7
Rahul Sinha <rahul@idiv.in>
2025-09-08 15:23:42 +0530

    Added bse64 key

--- scripts/modash/main.go
+++ scripts/modash/main.go
@@ -1,6 +1,6 @@
 package main
 
-const API_KEY = "rLkKfXn0uUXbFRbQPa85gz74MEVTR9oY"
+const API_KEY_BASE64 = "TG1kMDAwQWNXSHliQVFlcEVyRXBrTUxUYkZVUmVEZFg="
 
 func main() {
 	// Write functino to scrape all the username and use it later
