e06930129acbe4e83f7cf3b548e2365a3cbd3484
Rahul Sinha <rahul@idiv.in>
2025-09-10 01:30:24 +0530

    SQL is working

--- internal/trendly_discovery/discovery_test.go
+++ internal/trendly_discovery/discovery_test.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	trendlydiscovery "github.com/idivarts/backend-sls/internal/trendly_discovery"
 )
 
 func TestDiscovery(t *testing.T) {
-	sql := trendlydiscovery.FormSQL(trendlydiscovery.InfluencerFilters{})
+	sql := trendlydiscovery.FormSQL(trendlydiscovery.InfluencerFilters{
+		FollowerMin: aws.Int64(7000),
+		Name:        aws.String("Saks"),
+	})
 	log.Println(sql)
 }
