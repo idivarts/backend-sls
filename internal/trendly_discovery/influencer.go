8aef13ef01f466e3623e94e8919e7c36c4fb5d84
Rahul Sinha <rahul@idiv.in>
2025-09-16 20:46:44 +0530

    Added the needed function for analysis

--- internal/trendly_discovery/influencer.go
+++ internal/trendly_discovery/influencer.go
@@ -7,6 +7,28 @@ import (
 	"github.com/idivarts/backend-sls/internal/models/trendlybq"
 )
 
+type Range struct {
+	Min int `json:"min"`
+	Max int `json:"max"`
+}
+
+// If the influencer has practive to hide the views likes and comments ->
+
+func calculateTrustablity(social *trendlybq.Socials) int {
+
+	return 100
+}
+
+func calculateBudget(social *trendlybq.Socials) Range {
+
+	return Range{}
+}
+
+func calculateReach(social *trendlybq.Socials) Range {
+
+	return Range{}
+}
+
 // Make sure the the influencers discovery credit is reduced
 // If the influencer is already fetched before, do not reduce the credit
 // Also make sure the influencer is added uniquely to the user's list of influencers
@@ -23,7 +45,21 @@ func FetchInfluencer(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Cant fetch", "error": err.Error()})
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Fetched influencer", "social": social})
+	type CalculatedData struct {
+		Quality         int   `json:"quality"`
+		Trustablity     int   `json:"trustablity"`
+		EstimatedBudget Range `json:"estimatedBudget"`
+		EstimatedReach  Range `json:"estimatedReach"`
+	}
+
+	calculatedValue := CalculatedData{
+		Quality:         social.QualityScore,
+		Trustablity:     calculateTrustablity(social),
+		EstimatedBudget: calculateBudget(social),
+		EstimatedReach:  calculateReach(social),
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Fetched influencer", "social": social, "analysis": calculatedValue})
 }
 
 func RequestConnection(c *gin.Context) {
