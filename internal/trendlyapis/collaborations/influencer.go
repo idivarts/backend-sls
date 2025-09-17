6a4a54fb1cf3799456ac0d5d6ae133b0a5ce3283
Rahul Sinha <rahul@idiv.in>
2025-09-17 11:52:28 +0530

    Created a message channel

--- internal/trendlyapis/collaborations/influencer.go
+++ internal/trendlyapis/collaborations/influencer.go
@@ -1,12 +1,15 @@
 package trendlyCollabs
 
 import (
+	"context"
 	"net/http"
 
+	stream_chat "github.com/GetStream/stream-chat-go/v5"
 	"github.com/gin-gonic/gin"
 	"github.com/idivarts/backend-sls/internal/middlewares"
 	"github.com/idivarts/backend-sls/internal/models/trendlymodels"
 	"github.com/idivarts/backend-sls/pkg/myutil"
+	"github.com/idivarts/backend-sls/pkg/streamchat"
 )
 
 type requestWithBrands struct {
@@ -66,27 +69,6 @@ func SendMessage(c *gin.Context) {
 		return
 	}
 
-	// contract := trendlymodels.Contract{}
-	// err := contract.Get(contractId)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Error fetching contract"})
-	// 	return
-	// }
-
-	// collab := trendlymodels.Collaboration{}
-	// err = collab.Get(contract.CollaborationID)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Error fetching collaboration"})
-	// 	return
-	// }
-
-	// brand := trendlymodels.Brand{}
-	// err = brand.Get(contract.BrandID)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Error fetching Brand"})
-	// 	return
-	// }
-
 	user := trendlymodels.User{}
 	err := user.Get(influenerId)
 	if err != nil {
@@ -94,71 +76,40 @@ func SendMessage(c *gin.Context) {
 		return
 	}
 
-	// // Send Push Notification
-	// notif := &trendlymodels.Notification{
-	// 	Title:       fmt.Sprintf("You are connected with brand"),
-	// 	Description: "You can now find the details on the contract's menu",
-	// 	IsRead:      false,
-	// 	Data: &trendlymodels.NotificationData{
-	// 		UserID: &managerId,
-	// 	},
-	// 	TimeStamp: time.Now().UnixMilli(),
-	// 	Type:      "contract-started",
-	// }
-	// _, emails, err := notif.Insert(trendlymodels.BRAND_COLLECTION, collab.BrandID)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	return
-	// }
-	// _, _, err = notif.Insert(trendlymodels.USER_COLLECTION, contract.UserID)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	return
-	// }
+	brand := &trendlymodels.Brand{}
+	err = brand.Get(req.BrandId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Error fetching brand"})
+		return
+	}
 
-	// // Send Email notification
+	has := false
+	for i := range brand.UnlockedInfluencers {
+		if brand.UnlockedInfluencers[i] == influenerId {
+			has = true
+			break
+		}
+	}
 
-	// // 	<!--
-	// //   Dynamic Variables:
-	// // {{.RecipientName}}     => Name of the recipient (Brand or Influencer)
-	// // {{.CollabTitle}}       => Title of the collaboration
-	// // {{.StartDate}}         => Date when the collaboration was started
-	// // {{.ContractLink}}      => Link to view the created contract
-	// // -->
+	if !has {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "influencer-locked", "message": "Influencer not unlocked"})
+		return
+	}
 
-	// if user.Email != nil {
-	// 	data := map[string]interface{}{
-	// 		"RecipientName": user.Name,
-	// 		"CollabTitle":   collab.Name,
-	// 		"StartDate":     mytime.FormatPrettyIST(time.Now()),
-	// 		"ContractLink":  fmt.Sprintf("%s/contract-details/%s", constants.TRENDLY_CREATORS_FE, contractId),
-	// 	}
-	// 	err = myemail.SendCustomHTMLEmail(*user.Email, templates.CollaborationStarted, templates.SubjectCollaborationStarted, data)
-	// 	if err != nil {
-	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// }
-	// if len(emails) > 0 {
-	// 	data := map[string]interface{}{
-	// 		"RecipientName": brand.Name,
-	// 		"CollabTitle":   collab.Name,
-	// 		"StartDate":     mytime.FormatPrettyIST(time.Now()),
-	// 		"ContractLink":  fmt.Sprintf("%s/contract-details/%s", constants.TRENDLY_BRANDS_FE, contractId),
-	// 	}
-	// 	err = myemail.SendCustomHTMLEmailToMultipleRecipients(emails, templates.CollaborationStarted, templates.SubjectCollaborationStarted, data)
-	// 	if err != nil {
-	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// }
+	channel, err := streamchat.Client.CreateChannel(context.Background(), "messaging", "", managerId, &stream_chat.ChannelRequest{
+		Members: []string{managerId, influenerId},
+	})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Error creating channel"})
+		return
+	}
 
-	// // Send Stream Notification
-	// err = streamchat.SendSystemMessage(contract.StreamChannelID, "Congratulations!! The contract has been started!\nYou can find the contract details on the contract menu")
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Stream Error"})
-	// 	return
-	// }
+	// Send Stream Notification
+	err = streamchat.SendSystemMessage(channel.Channel.ID, "This message is initiated as the brand "+brand.Name+" wanted to connect with you")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Stream Error"})
+		return
+	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Successfully Notified for starting contract", "managerId": managerId, "influenerId": influenerId, "userEmail": user.Email})
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully Notified for starting contract", "channel": channel})
 }
