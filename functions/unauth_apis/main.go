d5add3f05c9166b8523489b5399e1fac7bb43a81
Rahul Sinha <rahul@idiv.in>
2025-12-25 15:01:12 +0530

    Added comments to understand better

--- functions/unauth_apis/main.go
+++ functions/unauth_apis/main.go
@@ -8,10 +8,19 @@ import (
 func main() {
 	instaApi := apihandler.GinEngine.Group("/instagram")
 
+	// This is called by frontend. Purpose is to just redirect to insta auth url with needed params
 	instaApi.GET("/", trendlyunauth.InstagramRedirect)
+
+	// From insta server we get redirected here with code -> we inturn we redirect to frontend with code
 	instaApi.GET("/auth/:redirect_type", trendlyunauth.InstagramAuthRedirect)
+
+	// From frontend we call this api with code to complete the auth process and save tokens
 	instaApi.POST("/auth", trendlyunauth.InstagramAuth)
+
+	// Insta calls this api to deauthorize our app for a user
 	instaApi.GET("/deauth", trendlyunauth.InstagramDeAuth)
+
+	// Insta calls this api to delete our app for a user
 	instaApi.GET("/delete", trendlyunauth.InstagramDelete)
 
 	firebaseApi := apihandler.GinEngine.Group("/firebase")
