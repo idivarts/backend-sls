447bf4c8c744d16efacfd52607599f9e500ac602
Rahul Sinha <rahul@idiv.in>
2026-01-26 16:16:22 +0530

    Added the basic sctruct which would change as per needed

--- internal/models/trendlymodels/contract.go
+++ internal/models/trendlymodels/contract.go
@@ -15,6 +15,40 @@ type Contract struct {
 	Status          int    `json:"status" firestore:"status"`
 
 	// All Items for storing the monetization related data
+	Payment struct {
+		OrderID         string   `json:"orderId,omitempty" firestore:"orderId,omitempty"`
+		Status          string   `json:"status,omitempty" firestore:"status,omitempty"`
+		PaymentID       string   `json:"paymentId,omitempty" firestore:"paymentId,omitempty"`
+		PaymentWebhooks []string `json:"paymentWebhooks,omitempty" firestore:"paymentWebhooks,omitempty"`
+	} `json:"payment" firestore:"payment"`
+
+	Shipment struct {
+		ShipmentID      string   `json:"shipmentId,omitempty" firestore:"shipmentId,omitempty"`
+		Status          string   `json:"status,omitempty" firestore:"status,omitempty"`
+		ShipmentUpdates []string `json:"shipmentUpdates,omitempty" firestore:"shipmentUpdates,omitempty"`
+	} `json:"shipment" firestore:"shipment"`
+
+	Deliverable struct {
+		DeliverableID    string   `json:"deliverableId,omitempty" firestore:"deliverableId,omitempty"`
+		Status           string   `json:"status,omitempty" firestore:"status,omitempty"`
+		DeliverableLinks []string `json:"deliverableLinks,omitempty" firestore:"deliverableLinks,omitempty"`
+		ApprovalRequests []string `json:"approvalRequests,omitempty" firestore:"approvalRequests,omitempty"`
+	} `json:"deliverable" firestore:"deliverable"`
+
+	Posting struct {
+		ScheduledDate      int64    `json:"scheduledDate,omitempty" firestore:"scheduledDate,omitempty"`
+		Status             string   `json:"status,omitempty" firestore:"status,omitempty"`
+		RescheduleRequests []string `json:"rescheduleRequests,omitempty" firestore:"rescheduleRequests,omitempty"`
+		PostedLinks        []string `json:"postedLinks,omitempty" firestore:"postedLinks,omitempty"`
+	} `json:"posting" firestore:"posting"`
+
+	Analytics struct {
+		Views       int `json:"views,omitempty" firestore:"views,omitempty"`
+		Likes       int `json:"likes,omitempty" firestore:"likes,omitempty"`
+		Comments    int `json:"comments,omitempty" firestore:"comments,omitempty"`
+		Shares      int `json:"shares,omitempty" firestore:"shares,omitempty"`
+		Impressions int `json:"impressions,omitempty" firestore:"impressions,omitempty"`
+	} `json:"analytics" firestore:"analytics"`
 }
 
 func (b *Contract) Get(contractID string) error {
