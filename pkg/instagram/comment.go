2c6eb873fcd84ddafc8d98b14cb109eba1a51dc4
Rahul Sinha <rahul@idiv.in>
2026-06-06 22:59:27 +0530

    Added comment reply stuff

--- pkg/instagram/comment.go
+++ pkg/instagram/comment.go
@@ -74,3 +74,46 @@ func GetComments(mediaID, accessToken string, params IGetCommentsParams) ([]Inst
 	}
 	return data.Data, nil
 }
+
+type CommentReplyResponse struct {
+	ID string `json:"id"`
+}
+
+// ReplyToComment posts a reply to an existing top-level comment.
+// Docs: https://developers.facebook.com/docs/instagram-api/reference/ig-comment/replies
+// graphType: 0 = Facebook Graph (page-scoped access token), non-zero = Instagram Graph.
+func ReplyToComment(commentID, message, accessToken string, graphType int) (*CommentReplyResponse, error) {
+	client := http.Client{}
+
+	apiURL := fmt.Sprintf("%s/%s/%s/replies", BaseURL, ApiVersion, commentID)
+	if graphType == 0 {
+		apiURL = fmt.Sprintf("%s/%s/%s/replies", messenger.BaseURL, messenger.ApiVersion, commentID)
+	}
+
+	iParam := url.Values{}
+	iParam.Set("message", message)
+	iParam.Set("access_token", accessToken)
+
+	apiURL = fmt.Sprintf("%s?%s", apiURL, iParam.Encode())
+
+	resp, err := client.Post(apiURL, "application/x-www-form-urlencoded", nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("Error: Unexpected status code - " + resp.Status + "\n" + string(body))
+	}
+
+	data := CommentReplyResponse{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
