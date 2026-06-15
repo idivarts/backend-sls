f2d88f18fe9ada764746216ec6f7102bdcf0c906
Rahul Sinha <rahul@idiv.in>
2026-06-15 14:29:55 +0530

    Firestore split dev to prod

--- pkg/firebase/firestore/init.go
+++ pkg/firebase/firestore/init.go
@@ -3,19 +3,26 @@ package firestoredb
 import (
 	"context"
 	"log"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	firebaseapp "github.com/idivarts/backend-sls/pkg/firebase"
+	"google.golang.org/api/option"
 )
 
 var Client *firestore.Client
 
 func init() {
 	ctx := context.Background()
-	var err error
-	log.Println("Creating Firestore")
 
-	Client, err = firebaseapp.FirebaseApp.Firestore(ctx)
+	dbID := os.Getenv("FIRESTORE_DATABASE_ID")
+	if dbID == "" {
+		dbID = "(default)"
+	}
+	log.Printf("Creating Firestore (project=%s db=%s)", firebaseapp.ProjectID, dbID)
+
+	var err error
+	Client, err = firestore.NewClientWithDatabase(ctx, firebaseapp.ProjectID, dbID, option.WithCredentialsFile(firebaseapp.ConfigFile))
 	if err != nil {
 		log.Fatalln(err)
 		panic(err.Error())
